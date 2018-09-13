bf2736bcb03e1cdc4043676bbb99fa790cdb2274
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-09-13 16:04:42 +0200

    Operators Done
    Operators are finished and should work corretcly if I understood correctly what they were intended for.

--- DevelopmentVec3Homework/DevelopmentVec3Homework/Vec3.h
+++ DevelopmentVec3Homework/DevelopmentVec3Homework/Vec3.h
@@ -11,12 +11,38 @@ public:
 	~vec3() {}
 
 	//OPERATORS (+,-,+=,-=,=,==)
-	void operator+  (const vec3 v2) {}
-	void operator-  (const vec3 v2) {}
-	void operator+= (const vec3 v2) {}
-	void operator-= (const vec3 v2) {}
-	void operator=  (const vec3 v2) {}
-	void operator== (const vec3 v2) {}
+	vec3 operator+  (const vec3 v2) 
+	{ 
+		vec3 vecret;
+		vecret.x = x + v2.x;
+		vecret.y = y + v2.y;
+		vecret.z = z + v2.z;
+		return vecret;
+	}
+	vec3 operator-  (const vec3 v2) 
+	{
+		vec3 vecret;
+		vecret.x = x - v2.x;
+		vecret.y = y - v2.y;
+		vecret.z = z - v2.z;
+		return vecret;
+	}
+	void operator+= (const vec3 v2) 
+	{
+		x += v2.x;
+		y += v2.y;
+		z += v2.z;
+	}
+	void operator-= (const vec3 v2) 
+	{
+		x += v2.x;
+		y += v2.y;
+		z += v2.z;
+	}
+	void operator=  (const vec3 v2) {/*X*/x = v2.x;/*Y*/ y = v2.y;/*Z*/ z = v2.z;}
+	
+	bool operator== (const vec3 v2) { return (x == v2.x && y == v2.y && z == v2.z);}
+	
 		
 	//METHODS
 	void normalize() {}
