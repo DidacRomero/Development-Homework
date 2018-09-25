9eac77a154d7cccdfdb8ba6a22e6c6f3f429490a
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-09-26 00:23:13 +0200

    String Class: Declared Operators

--- String Class/String Class/String.cpp
+++ String Class/String Class/String.cpp
@@ -1,4 +1,29 @@
 #include "String.h"
 
-void string::start()
+//CONSTRUCTORS
+String::String() : string(nullptr), string_char_num(0) {}//Size of the string(0){}
+
+String::String(char* string): string(string), string_char_num(0)
+{
+}
+
+String::String(const String &string_class): string(string_class.string), string_char_num(string_class.string_char_num) {}
+
+//DESTRUCTOR
+String::~String() {}
+
+//METHODS
+bool String::empty() const { return (string_char_num==0); }
+int String::size() const { return string_char_num; }
+void String::clear() {}
+const char* String::pointer() const { return string; }
+
+//OPERATORS
+String String::operator+(const String &string_class) 
+{}
+String String::operator+=(const String &string_class) 
+{}
+String String::operator=(const String &string_class) 
+{}
+String String::operator==(const String &string_class) 
 {}
--- String Class/String Class/String.h
+++ String Class/String Class/String.h
@@ -2,31 +2,34 @@
 #define _STRING__H_
 
 #include <cstring>
-class string
+class String
 {
 public:
 
-	void start();
+	String();
+	String(char* string);
+	String(const String &string_class);
 
+	~String();
 
+	//METHODS
+	bool empty() const;
+	int size() const;
+	void clear();
+	const char* pointer() const;
 
+	//OPERATORS  // +,+=,==, =
 
-
-
-
-
+	String operator+(const String &string_class);
+	String operator+= (const String &string_class);
+	String operator= (const String &string_class);
+	String operator== (const String &string_class);
 
 
 private:
 
-
-
-
-
-
-
-
-
+	char* string; //Pointer to the array of chars
+	int string_char_num; //Size of the string
 };
 
 
