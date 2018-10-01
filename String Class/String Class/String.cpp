292525e83ebff5603e052841d6ae20689fc2c0c5
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-10-02 01:41:06 +0200

    implemented all operators and constructors except string +

--- String Class/String Class/String.cpp
+++ String Class/String Class/String.cpp
@@ -1,29 +1,54 @@
 #include "String.h"
 
 //CONSTRUCTORS
-String::String() : string(nullptr), string_char_num(0) {}//Size of the string(0){}
+String::String() : string_mem_size(0) {}//Size of the string(0){}
 
-String::String(char* string): string(string), string_char_num(0)
+String::String(char* new_string): string_mem_size(strlen(new_string)), string(new char[string_mem_size])
 {
+	strcpy_s(string, (string_mem_size +1), new_string);
 }
 
-String::String(const String &string_class): string(string_class.string), string_char_num(string_class.string_char_num) {}
+String::String(const String &string_class): String(string_class.string) {}
 
 //DESTRUCTOR
-String::~String() {}
+String::~String() 
+{
+	clear();
+}
 
 //METHODS
-bool String::empty() const { return (string_char_num==0); }
-int String::size() const { return string_char_num; }
-void String::clear() {}
+bool String::empty() const { return (string_mem_size == 0); }
+int String::size() const { return string_mem_size; }
+void String::clear() 
+{
+	if (string != nullptr)
+	{
+		delete[] string;
+		string = nullptr;
+		string_mem_size = 0u;
+	}
+}
 const char* String::pointer() const { return string; }
 
 //OPERATORS
 String String::operator+(const String &string_class) 
-{}
-String String::operator+=(const String &string_class) 
-{}
-String String::operator=(const String &string_class) 
-{}
-String String::operator==(const String &string_class) 
-{}
+{
+	return strcat(string, string_class.string);
+}
+void String::operator+=(const String &string_class) 
+{
+	/*string_mem_size += string_class.string_mem_size;
+	char* new_string = new char[string_mem_size+1];
+	strcpy_s(string, (string_mem_size + 1));*/
+}
+void String::operator=(const String &string_class) 
+{
+	clear();
+	string_mem_size = string_class.string_mem_size;
+	string = new char[string_mem_size + 1];
+	strcpy(string,string_class.string);
+}
+bool String::operator==(const String &string_class) 
+{
+	return (strcmp(string, string_class.string) == 0 && string_mem_size == string_class.string_mem_size);
+}
--- String Class/String Class/String.h
+++ String Class/String Class/String.h
@@ -1,6 +1,8 @@
 #ifndef _STRING__H_
 #define _STRING__H_
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <cstring>
 class String
 {
@@ -21,15 +23,15 @@ public:
 	//OPERATORS  // +,+=,==, =
 
 	String operator+(const String &string_class);
-	String operator+= (const String &string_class);
-	String operator= (const String &string_class);
-	String operator== (const String &string_class);
+	void operator+= (const String &string_class);
+	void operator= (const String &string_class);
+	bool operator== (const String &string_class);
 
 
 private:
 
-	char* string; //Pointer to the array of chars
-	int string_char_num; //Size of the string
+	char* string = nullptr; //Pointer to the array of chars
+	unsigned int string_mem_size; //Size of the string
 };
 
 
