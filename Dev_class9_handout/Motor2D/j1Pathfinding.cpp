9907d2f41c5d0e7e108d5d96aa5a621d170aa646
Dídac Romero <36265669+DidacRomero@users.noreply.github.com>
2018-11-06 11:48:11 +0100

    Handout 9 Added + solving diagonals
    Thank you Aitor

--- Dev_class9_handout/Motor2D/j1Pathfinding.cpp
+++ Dev_class9_handout/Motor2D/j1Pathfinding.cpp
@@ -124,29 +124,12 @@ uint PathNode::FindWalkableAdjacents(PathList& list_to_fill) const
 	cell.create(pos.x, pos.y + 1);
 	if(App->pathfinding->IsWalkable(cell))
 		list_to_fill.list.add(PathNode(-1, -1, cell, this));
-	// north east
-	cell.create(pos.x +1, pos.y + 1);
-	if (App->pathfinding->IsWalkable(cell))
-		list_to_fill.list.add(PathNode(30, -1, cell, this));
-	// north west
-	cell.create(pos.x -1, pos.y + 1);
-	if (App->pathfinding->IsWalkable(cell))
-		list_to_fill.list.add(PathNode(30, -1, cell, this));
 
 	// south
 	cell.create(pos.x, pos.y - 1);
 	if(App->pathfinding->IsWalkable(cell))
 		list_to_fill.list.add(PathNode(-1, -1, cell, this));
 
-	// south east
-	cell.create(pos.x + 1, pos.y - 1);
-	if (App->pathfinding->IsWalkable(cell))
-		list_to_fill.list.add(PathNode(30, -1, cell, this));
-
-	//south west
-	cell.create(pos.x - 1, pos.y - 1);
-	if (App->pathfinding->IsWalkable(cell))
-		list_to_fill.list.add(PathNode(30, -1, cell, this));
 	// east
 	cell.create(pos.x + 1, pos.y);
 	if(App->pathfinding->IsWalkable(cell))
@@ -174,8 +157,8 @@ int PathNode::Score() const
 int PathNode::CalculateF(const iPoint& destination)
 {
 	g = parent->g + 1;
-	//h = pos.DistanceTo(destination);
-	h = pos.DistanceNoSqrt(destination);
+	h = pos.DistanceTo(destination);
+
 	return g + h;
 }
 
@@ -184,88 +167,10 @@ int PathNode::CalculateF(const iPoint& destination)
 // ----------------------------------------------------------------------------------
 int j1PathFinding::CreatePath(const iPoint& origin, const iPoint& destination)
 {
-	// TODO 1: if origin or destination are not walkable, return -1
-	if (IsWalkable(origin) == false || IsWalkable(destination) == false)
-	{
-		return -1;
-	}
+	int ret = -1;
 
-	// TODO 2: Create two lists: open, close
-	// Add the origin tile to open
-	// Iterate while we have tile in the open list
-	PathList open;
-	PathList closed;
-	PathList adjacents;
+	// Nice try :)
 
-	PathNode origin_node = {0,0,origin,nullptr};
-	
-	open.list.add(origin_node);
-
-	//p2List_item<PathNode> aux_path_node;
-	p2List_item<PathNode>* aux_pathNode;
-
-	while (open.list.count() > 0)
-	{
-		// TODO 3: Move the lowest score cell from open list to the closed list
-		aux_pathNode = open.GetNodeLowestScore();
-	
-		p2List_item<PathNode>* lowest_pathNode = closed.list.add(aux_pathNode->data);
-		open.list.del(aux_pathNode);
-
-	// TODO 4: If we just added the destination, we are done!
-	// Backtrack to create the final path
-	// Use the Pathnode::parent and Flip() the path when you are finish
-		if (lowest_pathNode->data.pos == destination)
-		{
-			last_path.Clear();
-			//Do the backtracking
-			// We will be iterating with parents which are const Pathnode *
-			 const PathNode* backTrackNode = &lowest_pathNode->data;
-
-			while (backTrackNode->pos != origin)
-			{
-				last_path.PushBack(backTrackNode->pos);
-				backTrackNode = backTrackNode->parent;
-			}
-			last_path.Flip();
-			return last_path.Count();	//We finished creating the Path so we end the whole loop
-		}
-
-		// TODO 5: Fill a list of all adjancent nodes
-		adjacents.list.clear(); //We need to clear for each iteration as the adjacents will be different for each tile
-		lowest_pathNode->data.FindWalkableAdjacents(adjacents);
-
-		// TODO 6: Iterate adjancent nodes:
-		
-		for (p2List_item<PathNode>* item = adjacents.list.start; item; item = item->next)
-		{
-			// Ignore nodes in the closed list
-			if (closed.Find(item->data.pos) == NULL)
-			{
-				p2List_item<PathNode>* adjacent_pathNode = open.Find(item->data.pos);
-				// If it is NOT found, calculate its F and add it to the open list
-				if (adjacent_pathNode == NULL)
-				{
-					//item->data.parent = &aux_pathNode->data;
-					item->data.CalculateF(destination);
-
-					//Check if it's a diagonal movement
-					if (abs(item->data.pos.x - item->data.parent->pos.x) == abs(item->data.pos.y - item->data.parent->pos.y))
-					{
-						item->data.g += 1;
-						open.list.add(item->data);
-						continue;
-					}
-					open.list.add(item->data);
-				}
-				// If it is already in the open list, check if it is a better path (compare G)
-				else if (adjacent_pathNode->data.g > item->data.g + 30) // If it is a better path, Update the parent
-				{
-					adjacent_pathNode->data.parent = item->data.parent;
-					adjacent_pathNode->data.CalculateF(destination);
-				}
-			}
-		}
-	}
+	return ret;
 }
 
